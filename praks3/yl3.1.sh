a21fccff1bdac19a9dd6088d23e618954c57c540
Egle Soosaar <egle.soosaar@khk.ee>
2021-11-04 14:09:29 +0000

    Nime küsimine

---
+++ praks3/yl3.1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# our comment is here
+
+echo -n  "Sisesta oma eesnimi ja perenimi: "
+read varname
+echo Tere tulemast, $varname
+echo -n "Sisesta oma eesnimi ja perenimi: "
+read nimi1 nimi2 
+echo Sinu eesnimi on $nimi1 ja perenimi on $nimi2  
+ 
+
+
+
