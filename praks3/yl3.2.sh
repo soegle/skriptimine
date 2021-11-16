244d17a7986c29f13747913e03b8357e7a47ba8a
Egle Soosaar <egle.soosaar@khk.ee>
2021-11-16 17:08:53 +0000

    Muutuja nimega lause

---
+++ praks3/yl3.2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#muutuja aasta arvuna 
+#muutuja president sõnena
+#muutuja lause_keskosa sõnena
+
+
+aasta=2016
+president="Kersti Kaljulaid"
+lause_keskosa=".aastal valiti Eesti presidendiks"
+lause="$aasta $lause_keskosa $president"
+echo $lause
+
