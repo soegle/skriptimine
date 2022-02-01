7548a66dbd125980058eafd4ffcf448bf710194b
Egle Soosaar <egle.soosaar@khk.ee>
2022-02-01 18:25:23 +0000

    Kuu number->nimi

---
+++ praks6/yl6.2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# peab arvutama numbrite summat ja väljastama koos selgitava tekstiga
+
+echo -n "Sisesta suvaline täisarv: "
+read arv
+summa=0
+while [ $arv -ne 0 ] 
+do
+	var1=$(($arv % 10))
+	summa=$((summa + $var1))
+	arv=$(($arv / 10))
+done
+echo $summa
